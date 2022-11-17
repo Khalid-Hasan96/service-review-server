0cbd6a4ee7a29d67b284bcdcb020c2b42e398d66
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-18 01:37:49 +0600

    services working

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c12imjm.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+      try {
+            const serviceCollection = client.db("serviceReviewdb").collection("services");
+
+            app.get('/services', async (req, res) => {
+                  const query = {};
+                  const cursor = serviceCollection.find(query);
+                  const services = await cursor.toArray();
+                  res.send(services)
+            })
+      }
+      finally {
+
+      }
+
+}
+run().catch(err => console.error(err))
+
 
 
 
