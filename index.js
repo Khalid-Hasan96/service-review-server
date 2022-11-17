f31ea5e5e889f741f5003b854bc60c0a9ece7036
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-17 18:01:34 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c12imjm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
       res.send('Review server is working')
