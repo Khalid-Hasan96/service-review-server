34cee2f95ef0c729dd7c309afb96ccb5b8335a0f
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-18 15:22:01 +0600

    serviceDetail with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -19,9 +19,22 @@ async function run() {
 
             app.get('/services', async (req, res) => {
                   const query = {};
+                  const cursor = serviceCollection.find(query).limit(3);
+                  const services = await cursor.toArray();
+                  res.send(services)
+            });
+            app.get('/allServices', async (req, res) => {
+                  const query = {};
                   const cursor = serviceCollection.find(query);
                   const services = await cursor.toArray();
                   res.send(services)
+            });
+
+            app.get('/serviceDetails/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: ObjectId(id) };
+                  const result = await serviceCollection.findOne(query);
+                  res.send(result);
             })
       }
       finally {
